29587e77eddf332a430c32291fd401744082a723
supercleanse <supercleanse@gmail.com>
2012-10-24 22:08:52 +0000

    Working with business routes now too

--- lib/Radiate360/business.rb
+++ lib/Radiate360/business.rb
@@ -2,18 +2,18 @@ Radiate360::Business = Struct.new :api_username, :api_key, :username, :business_
   include StructExtensions
 
   def pause(toggle)
-    Radiate360.call 'businesses/pause', {
-      :fields => {:username => self.username, :business_id => self.business_id, :pause => toggle},
-      :constraints => { :required => %w(username business_id pause) },
-      :credentials => { :username => :api_username, :password => :api_key }
+    Radiate360.call "businesses/#{self.business_id}/pause/#{toggle.to_s}", {
+      :fields => {:username => self.username},
+      :constraints => { :required => %w(username) },
+      :credentials => { :username => self.api_username, :password => self.api_key }
     }, :post
   end
   
   def bury(toggle)
-    Radiate360.call 'businesses/bury', {
-      :fields => {:username => self.username, :business_id => self.business_id, :bury => toggle},
-      :constraints => { :required => %w(username business_id bury) },
-      :credentials => { :username => :api_username, :password => :api_key }
+    Radiate360.call "businesses/#{self.business_id}/bury/#{toggle.to_s}", {
+      :fields => {:username => self.username},
+      :constraints => { :required => %w(username) },
+      :credentials => { :username => self.api_username, :password => self.api_key }
     }, :post
   end
 end
