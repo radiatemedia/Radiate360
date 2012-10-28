2cdefb201134af423f78d34af7bb210ad56f5ff0
Jeff Moss <jmoss@heavyobjects.com>
2012-10-28 04:00:14 -0600

    Rely on basic auth credentials

--- lib/Radiate360/business.rb
+++ lib/Radiate360/business.rb
@@ -4,16 +4,14 @@ Radiate360::Business = Struct.new :api_username, :api_key, :username, :business_
   def pause(toggle)
     Radiate360.call "businesses/#{self.business_id}/pause/#{toggle.to_s}", {
       :fields => {:username => self.username},
-      :constraints => { :required => %w(username) },
-      :credentials => { :username => self.api_username, :password => self.api_key }
+      :constraints => { :required => [] }
     }, :post
   end
   
   def bury(toggle)
     Radiate360.call "businesses/#{self.business_id}/bury/#{toggle.to_s}", {
       :fields => {:username => self.username},
-      :constraints => { :required => %w(username) },
-      :credentials => { :username => self.api_username, :password => self.api_key }
+      :constraints => { :required => [] }
     }, :post
   end
 end
